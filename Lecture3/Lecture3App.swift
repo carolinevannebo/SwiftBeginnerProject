ee83b32ad014dc7e29808c4d7dca50b099004712
carolinevannebo <58633333+carolinevannebo@users.noreply.github.com>
2023-09-04 14:27:59 +0200

    Initial Commit

---
+++ Lecture3/Lecture3App.swift
@@ -0,0 +1,17 @@
+//
+//  Lecture3App.swift
+//  Lecture3
+//
+//  Created by Caroline Vannebo on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Lecture3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
